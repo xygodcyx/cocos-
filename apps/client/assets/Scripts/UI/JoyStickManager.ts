4fea08b7dcb67c0d4be6f25eb9aa6117eab3fa91
XIANG <1323943635@qq.com>
2023-03-14 16:39:35 +0800

    cocos帧同步

---
+++ apps/client/assets/Scripts/UI/JoyStickManager.ts
@@ -0,0 +1,14 @@
+import { _decorator, Component, Node } from 'cc';
+const { ccclass, property } = _decorator;
+
+@ccclass('JoyStickManager')
+export class JoyStickManager extends Component {
+    start() {
+
+    }
+
+    update(deltaTime: number) {
+        
+    }
+}
+
